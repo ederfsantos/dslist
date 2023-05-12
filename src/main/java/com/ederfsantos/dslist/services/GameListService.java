e5dd48edbb88190ca0f6df87ede71ecc6ed94b65
EDER FERREIRA SANTOS <ederfsantos@live.com>
2023-05-12 00:59:48 -0300

    implementando o moveGames, alterando posicao do game no banco de dados, lista

--- src/main/java/com/ederfsantos/dslist/services/GameListService.java
+++ src/main/java/com/ederfsantos/dslist/services/GameListService.java
@@ -11,26 +11,45 @@ import com.ederfsantos.dslist.dto.GameListDTO;
 import com.ederfsantos.dslist.dto.GameMinDTO;
 import com.ederfsantos.dslist.entities.Game;
 import com.ederfsantos.dslist.entities.GameList;
+import com.ederfsantos.dslist.projections.GameMinProjection;
 import com.ederfsantos.dslist.repositories.GameListRepository;
-
-
+import com.ederfsantos.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
-	
-	@Transactional(readOnly = true)//nao vou bloquear para escrita o banco de dados
-	public List<GameListDTO> findAll(){
+
+	@Autowired
+	private GameRepository gameRepository;
+
+	@Transactional(readOnly = true) // nao vou bloquear para escrita o banco de dados
+	public List<GameListDTO> findAll() {
 		return gameListRepository.findAll().stream().map(GameListDTO::new).toList();
 		// ou da forma abaixo
-		//return gameRepository.findAll().stream().map(x-> new GameMinDTO(x)).toList();
+		// return gameRepository.findAll().stream().map(x-> new GameMinDTO(x)).toList();
 	}
-	
+
 	@Transactional(readOnly = true)
-	public GameListDTO findById( Long id) {
+	public GameListDTO findById(Long id) {
 		GameList result = gameListRepository.findById(id).get();
 		return new GameListDTO(result);
+	}
+
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+		for (int i = min; i <= max; i++) {
+
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+
+		}
 	}
 
 }
